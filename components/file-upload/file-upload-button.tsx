a1ad0514f6a6c11b16fb3a417ecd4c011611d086
jhyunwoo <jhyunwoo0228@gmail.com>
2024-08-23 14:26:51 +0900

    delete uppy from main branch

--- components/file-upload/file-upload-button.tsx
+++ components/file-upload/file-upload-button.tsx
@@ -1,30 +1,30 @@
-'use client';
+'use client'
 
-import { useEffect, useRef } from 'react';
-import { useRecoilValue } from 'recoil';
-import { filesState, shareTimeState } from '@/lib/recoil';
-import { useSession } from 'next-auth/react';
-import { ClientToWorkersMessageType } from '@/lib/types';
+import { useEffect, useRef } from 'react'
+import { useRecoilValue } from 'recoil'
+import { filesState, shareTimeState } from '@/lib/recoil'
+import { useSession } from 'next-auth/react'
+import { ClientToWorkersMessageType } from '@/lib/types'
 
 export default function FileUploadButton() {
-  const fileUploadWorker = useRef<Worker | null>(null);
-  const uploadFileList = useRecoilValue(filesState);
-  const shareTime = useRecoilValue(shareTimeState);
-  const { data: session } = useSession();
+  const fileUploadWorker = useRef<Worker | null>(null)
+  const uploadFileList = useRecoilValue(filesState)
+  const shareTime = useRecoilValue(shareTimeState)
+  const { data: session } = useSession()
 
   useEffect(() => {
-    fileUploadWorker.current = new Worker(new URL('./file-upload-worker.ts', import.meta.url), { type: 'module' });
+    fileUploadWorker.current = new Worker(new URL('./file-upload-worker.ts', import.meta.url), { type: 'module' })
 
     const handleMessage = (event: MessageEvent) => {
-      console.log(`${event.data} >> [Client] Received from Worker!`);
-    };
+      console.log(`${event.data} >> [Client] Received from Worker!`)
+    }
 
-    fileUploadWorker.current.addEventListener('message', handleMessage);
+    fileUploadWorker.current.addEventListener('message', handleMessage)
 
     return () => {
-      fileUploadWorker.current?.terminate();
-    };
-  }, []);
+      fileUploadWorker.current?.terminate()
+    }
+  }, [])
 
   return (
     <button
@@ -42,5 +42,5 @@ export default function FileUploadButton() {
     >
       공유
     </button>
-  );
+  )
 }
