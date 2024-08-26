b936c146ce57740210cadeee417f7edc845266ea
jhyunwoo <jhyunwoo0228@gmail.com>
2024-08-27 02:19:45 +0900

    add download button

--- app/search/[code]/page.tsx
+++ app/search/[code]/page.tsx
@@ -2,15 +2,31 @@ import decodeKoCode from '@/lib/decode-ko-code'
 import db from '@/db'
 import { eq } from 'drizzle-orm'
 import { share } from '@/db/schema'
-import { FolderOpenIcon, TrashIcon } from '@heroicons/react/24/outline'
+import { CloudArrowDownIcon, FolderOpenIcon, TrashIcon } from '@heroicons/react/24/outline'
 import ShareButton from '@/components/share-button'
 import HomePageButton from '@/components/homepage-button'
+import getS3Client from '@/lib/r2/get-s3-client'
+import { GetObjectCommand } from '@aws-sdk/client-s3'
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
 
 export default async function SearchPage({ params }: { params: { code: string } }) {
   const code = decodeKoCode(params.code)
   const shareData = await db.query.share.findFirst({
     where: eq(share.code, code),
   })
+  const r2client = getS3Client()
+  const urlRequests = []
+  if (shareData?.file) {
+    for (const file of shareData?.file) {
+      const downloadCommand = new GetObjectCommand({
+        Bucket: process.env.R2_BUCKET,
+        Key: `${shareData.id}/${file}`,
+        ResponseContentDisposition: `attachment; filename="${file}"`,
+      })
+      urlRequests.push(getSignedUrl(r2client, downloadCommand, { expiresIn: 60 * 60 * 24 }))
+    }
+  }
+  const downloadUrl = await Promise.all(urlRequests)
 
   if (shareData) {
     return (
@@ -25,9 +41,18 @@ export default async function SearchPage({ params }: { params: { code: string } 
             url={`${process.env.SITE_URL}/search/${code.replaceAll(' ', '_')}`}
           />
         </div>
-        <div className={'p-2 px-3 rounded-xl bg-neutral-900'}>
+        <div className={'p-2 rounded-xl '}>
           <div className={'text-xl font-semibold'}>Files</div>
-          <div>{shareData.file?.map((fileData, index) => <div key={index}>{fileData}</div>)}</div>
+          <div className={'flex flex-col gap-1'}>
+            {shareData.file?.map((fileData, index) => (
+              <div key={index} className={'flex items-center justify-between bg-neutral-900 p-2 px-4 rounded-lg'}>
+                <div>{fileData}</div>
+                <a href={downloadUrl[index]} download={fileData}>
+                  <CloudArrowDownIcon className={'size-8'} />
+                </a>
+              </div>
+            ))}
+          </div>
         </div>
         <div className={'w-full p-4 fixed bottom-0 left-0 flex'}>
           <div className={'flex gap-2 max-w-4xl w-full mx-auto'}>
