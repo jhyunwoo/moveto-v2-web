806cb57bef670ea0ce2bfd9c35380b17c56bce33
jhyunwoo <jhyunwoo0228@gmail.com>
2024-09-01 22:39:02 +0900

    add delete file on r2 code

--- app/api/admin/storage/route.ts
+++ app/api/admin/storage/route.ts
@@ -2,13 +2,30 @@ import db from '@/db'
 import { share } from '@/db/schema'
 import { and, eq, isNotNull, lt, or } from 'drizzle-orm'
 import { NextResponse } from 'next/server'
+import getS3Client from '@/lib/r2/get-s3-client'
+import { DeleteObjectsCommand } from '@aws-sdk/client-s3'
 
 export async function GET() {
   const expiringShares = await db
     .update(share)
     .set({ code: null, active: false })
     .where(and(lt(share.expireAt, new Date()), or(isNotNull(share.code), eq(share.active, true))))
-    .returning({ id: share.id })
+    .returning({ id: share.id, file: share.file })
 
-  return NextResponse.json({ expired: expiringShares.length })
+  const files: { Key: string }[] = []
+  for (const share of expiringShares) {
+    if (share.file) {
+      for (const file of share.file) {
+        files.push({ Key: `${share.id}/${file}` })
+      }
+    }
+  }
+  const client = getS3Client()
+  const command = new DeleteObjectsCommand({
+    Bucket: process.env.R2_BUCKET!,
+    Delete: { Objects: files },
+  })
+  const result = await client.send(command)
+
+  return NextResponse.json({ expired: expiringShares.length, result })
 }
