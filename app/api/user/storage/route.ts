b259e05b0bed1f071fc733a9d09c805e2e5c36b4
jhyunwoo <jhyunwoo0228@gmail.com>
2024-09-05 14:03:35 +0900

    fix api cache

--- app/api/user/storage/route.ts
+++ app/api/user/storage/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from 'next/server'
 import getUsedStorage from '@/lib/get-used-storage'
 
+export const dynamic = 'force-dynamic'
+
 export async function GET() {
   return NextResponse.json({ storageSize: await getUsedStorage() })
 }
