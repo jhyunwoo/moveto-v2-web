d3997aee420c9f72b40621797b2237fffce8ff66
jhyunwoo <jhyunwoo0228@gmail.com>
2024-09-05 11:29:16 +0900

    add pwa

---
+++ app/api/user/shares/route.ts
@@ -0,0 +1,20 @@
+import { auth } from '@/auth'
+import db from '@/db'
+import { share } from '@/db/schema'
+import { NextResponse } from 'next/server'
+import { and, desc, eq } from 'drizzle-orm'
+
+export async function GET() {
+  const session = await auth()
+  if (!session) {
+    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
+  }
+
+  const shareList = await db
+    .select()
+    .from(share)
+    .where(and(eq(share.userId, session?.user.id!), eq(share.active, true)))
+    .orderBy(desc(share.createdAt))
+
+  return NextResponse.json(shareList)
+}
--- app/profile/history/page.tsx
+++ app/profile/history/page.tsx
@@ -1,36 +1,29 @@
-import db from '@/db'
-import { share } from '@/db/schema'
-import { and, eq } from 'drizzle-orm'
-import { auth } from '@/auth'
+'use client'
+
 import Link from 'next/link'
 import { ChevronLeftIcon } from '@heroicons/react/24/outline'
 import ConfirmShareDelete from '@/components/confirm-share-delete'
 import DeleteShareTrashIconButton from '@/components/delete-share-trash-icon-button'
+import useShares from '@/lib/hooks/useShares'
+import { motion } from 'framer-motion'
 
-export const dynamic = 'force-dynamic'
-
-export default async function HistoryPage() {
-  const session = await auth()
-
-  const shareList = await db
-    .select()
-    .from(share)
-    .where(and(eq(share.userId, session?.user.id!), eq(share.active, true)))
-
-  function dateToKor(date: Date) {
-    const options: { dateStyle: 'medium'; timeStyle: 'short'; hour12: boolean } = {
-      dateStyle: 'medium',
-      timeStyle: 'short',
-      hour12: false,
-    }
-    return Intl.DateTimeFormat('ko-KR', options).format(date)
+function dateToKor(date: Date) {
+  const options: { dateStyle: 'medium'; timeStyle: 'short'; hour12: boolean } = {
+    dateStyle: 'medium',
+    timeStyle: 'short',
+    hour12: false,
   }
+  return Intl.DateTimeFormat('ko-KR', options).format(date)
+}
 
-  function isExpired(expiredAt: Date | null) {
-    if (!expiredAt) return false
+function isExpired(expiredAt: Date | null) {
+  if (!expiredAt) return false
 
-    return new Date() > expiredAt
-  }
+  return new Date() > expiredAt
+}
+
+export default function HistoryPage() {
+  const { shares } = useShares()
 
   return (
     <div className={'text-white'}>
@@ -40,10 +33,16 @@ export default async function HistoryPage() {
         <div className={'group-hover:underline'}>프로필 페이지</div>
       </Link>
       <div className={'text-xl font-semibold py-4'}>파일 공유 기록</div>
-      {shareList.length === 0 && <div className={'text-center p-4'}>아직 공유한 파일이 없습니다.</div>}
+      {shares?.length === 0 && <div className={'text-center p-4'}>아직 공유한 파일이 없습니다.</div>}
       <div className={'grid grid-cols-1 md:grid-cols-2 gap-2'}>
-        {shareList.map(share => (
-          <div key={share.id} className={'p-2 rounded-lg bg-neutral-900 flex flex-col gap-2'}>
+        {shares?.map(share => (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            key={share.id}
+            className={'p-2 rounded-lg bg-neutral-900 flex flex-col gap-2'}
+          >
             <div className={'flex justify-between items-start'}>
               <div className={'break-all'}>
                 {share.file ? share.file[0] : null}
@@ -61,11 +60,11 @@ export default async function HistoryPage() {
                 </Link>
               ) : null}
               <div className={'ml-auto'}>
-                <div className={'text-sm'}>{dateToKor(share.createdAt)} 생성</div>
-                <div className={'text-sm'}>{dateToKor(share.expireAt!)} 만료</div>
+                <div className={'text-sm'}>{dateToKor(new Date(share.createdAt))} 생성</div>
+                <div className={'text-sm'}>{dateToKor(new Date(share.expireAt!))} 만료</div>
               </div>
             </div>
-          </div>
+          </motion.div>
         ))}
       </div>
     </div>
