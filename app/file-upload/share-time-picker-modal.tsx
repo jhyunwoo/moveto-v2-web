c6f9b8c6134f9c70fd6719f3c39feb74af0c3dcc
jhyunwoo <jhyunwoo0228@gmail.com>
2024-09-08 01:40:28 +0900

    refactoring file upload component

---
+++ app/file-upload/share-time-picker-modal.tsx
@@ -0,0 +1,46 @@
+import { useRecoilState } from 'recoil'
+import { shareTimeState, shareTimePopUpState } from '@/lib/client/recoil'
+import { useSession } from 'next-auth/react'
+import { useEffect } from 'react'
+import FileUploadButton from '@/app/file-upload/file-upload-button'
+import ModalLayout from '@/components/modal-layout'
+import getShareTimeOptionsForPlan from '@/lib/get-share-time-options-for-plan'
+
+export default function ShareTimePickerModal({ uploadFunc }: { uploadFunc: () => void }) {
+  const [shareTimePopUp, setShareTimePopUp] = useRecoilState(shareTimePopUpState)
+  const [shareTime, setShareTime] = useRecoilState(shareTimeState)
+  const session = useSession()
+
+  const planShareTime = getShareTimeOptionsForPlan(session.data?.user.plan)
+
+  useEffect(() => {
+    setShareTime(planShareTime[0]?.value)
+  }, [planShareTime, setShareTime])
+
+  return (
+    <ModalLayout isOpen={shareTimePopUp} closeModal={() => setShareTimePopUp(false)}>
+      <div className={'py-2 text-2xl font-semibold'}>공유 시간</div>
+      <div className={'grid grid-cols-3 grid-rows-3 gap-2'}>
+        {planShareTime.map(data => (
+          <button
+            onClick={() => setShareTime(data.value)}
+            key={data.text}
+            className={`rounded-lg p-3 ${shareTime === data.value ? 'bg-neutral-100 text-black' : 'bg-neutral-800 text-neutral-50'} flex items-center justify-center transition-colors`}
+          >
+            <div>{data.text}</div>
+          </button>
+        ))}
+      </div>
+      <div className={'mt-4 flex w-full items-center gap-2'}>
+        <button
+          className={'w-auto rounded-full border-2 border-red-600 p-2 px-4 text-red-500'}
+          type={'button'}
+          onClick={() => setShareTimePopUp(false)}
+        >
+          취소
+        </button>
+        <FileUploadButton uploadFunc={uploadFunc} />
+      </div>
+    </ModalLayout>
+  )
+}
