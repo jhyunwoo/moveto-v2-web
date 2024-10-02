63bc2701a7934e7bb4fc156c71ed9a0887df9979
jhyunwoo <jhyunwoo0228@gmail.com>
2024-10-02 10:18:59 +0900

    delete unnecessary hook

--- app/file-upload/shareable-file-size.tsx
+++ app/file-upload/shareable-file-size.tsx
@@ -9,14 +9,13 @@ import { useEffect } from 'react'
 import useUsedStorage from '@/lib/hooks/use-used-storage'
 import ShareableFileSizeBar from '@/components/shareable-file-size-bar'
 import SharableFileSizeInfo from '@/components/sharable-file-size-info'
-import useUserPlan from '@/lib/hooks/use-user-plan'
 
 export default function ShareableFileSize() {
   const totalSize = useRecoilValue(totalFileSizeState)
   const setDisableUpload = useSetRecoilState(disableUploadState)
 
   const session = useSession()
-  const userPlan = useUserPlan()
+  const userPlan = session.data?.user.plan ? `${session.data.user.plan} Plan` : '미인증 사용자'
   const { usedStorage, usedStorageLoading } = useUsedStorage()
 
   const userShareLimit = getUserLimit(session.data?.user.plan)
--- lib/hooks/use-user-plan.ts
+++
@@ -1,6 +0,0 @@
-import { useSession } from 'next-auth/react'
-
-export default function useUserPlan() {
-  const session = useSession()
-  return session.data?.user.plan ? `${session.data.user.plan} Plan` : '미인증 사용자'
-}
