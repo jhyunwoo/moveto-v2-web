c3b4e60c67b179d4763684930b096c9c09240bdf
jhyunwoo <jhyunwoo0228@gmail.com>
2025-03-18 19:38:12 +0900

    add GA

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({ children }: Readonly<{ children: ReactNode 
         <AuthProvider>{children}</AuthProvider>
         <Footer />
       </body>
-      <GoogleAnalytics gaId={'G-XRMLPE2V9S'} />
+      <GoogleAnalytics gaId={'G-BVNJYWQGEF'} />
     </html>
   )
 }
