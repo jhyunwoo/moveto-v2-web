ad4266a00d7adb6b67e00801dbc8d1e38f4169c4
2024148005 <eel_07applets@icloud.com>
2025-04-17 14:50:09 +0900

    fix drag and drop ui

--- app/file-upload/drag-and-drop-box.tsx
+++ app/file-upload/drag-and-drop-box.tsx
@@ -15,7 +15,7 @@ export default function DragAndDropBox({
   })
   return (
     <div
-      className={`relative h-[30vh] w-full rounded-xl border-2 border-dashed border-white transition hover:bg-neutral-900`}
+      className={`relative h-[30vh] w-full rounded-xl border-2 border-dashed border-neutral-500  transition hover:bg-neutral-900`}
     >
       <input
         ref={inputRef}
