195aa50a1adb2590979febcf637eb6a5f617d354
jhyunwoo <jhyunwoo0228@gmail.com>
2025-11-21 11:46:51 +0900

    fix worker error

--- lib/worker/file-upload-worker.ts
+++ lib/worker/file-upload-worker.ts
@@ -3,7 +3,7 @@ import fetchJson from '@/lib/client/fetch-json'
 import { Meta, Uppy } from '@uppy/core'
 import AwsS3, { AwsBody } from '@uppy/aws-s3'
 
-const BASE_URL = 'http://localhost:3000'
+const BASE_URL = 'https://www.moveto.kr'
 
 async function createShare(fileNameList: string[], storageSize: number) {
   const bodyData: { files: string[]; storageSize: number } = {
